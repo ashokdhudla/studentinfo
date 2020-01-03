4ff8a272a1c294729a9b3628e09ce26304cc5ec8
abhilash413 <abhi040990@gmail.com>
2020-01-03 09:06:09 +0530

    worked on registration.html

--- student/views.py
+++ student/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def login(request):
     return render(request,'login.html')
 
+
 def registration(request):
+    print(request)
+    if request.method == "POST":
+        print("it's a POST method")
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        birthday = request.POST["Birthday"]
+        gender = request.POST["gender"]
+        submit = request.POST["submit"]
+        reset = request.POST["reset"]
+        print(firstname, lastname, email, password, birthday, gender, submit, reset)
+
     return render(request,'registration.html')
+
